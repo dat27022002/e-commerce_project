5d75f9a65ca6a7be95f52743aae9af06aea067c3
TrangPhan77 <trang.phan@eyeclick.com>
2024-04-12 00:44:42 +0700

    feat/login: update profile

--- src/components/profile/ProfileComponent.js
+++ src/components/profile/ProfileComponent.js
@@ -11,107 +11,172 @@ import {
     RadioGroup,
     FormControlLabel,
     Radio,
+    Button,
+    CardActions,
+    FormControl
 } from '@mui/material';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Controller, useForm } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
+import { useEffect, useRef } from 'react';
+import httpRequest from 'src/utils/httpRequest';
+import {toast} from 'react-toastify';
 
 const schema = yup.object().shape({
-    name: yup.string(),
+    name: yup.string().required(),
     email: yup.string().email().required(),
     phone_number: yup
         .string()
         .matches(/^[0-9]{10,11}$/, 'Phone number is not valid')
         .required(),
-    gender: yup.string(),
-    birthday: yup.string(),
-    image: yup.string(),
+    sex: yup.string().required(),
+    date_of_birth: yup.string().required(),
+    image: yup.string().required(),
 });
-const ProfileComponent = () => {
+const ProfileComponent = ({setName}) => {
     const {
         handleSubmit,
         control,
         formState: { errors },
         setError,
+        setValue,
+        getValues,
+        watch
     } = useForm({
         resolver: yupResolver(schema),
     });
+    const inputRef = useRef(null)
+    const uploadImage = async (value) => {
+        const file = value.target.files[0]
+        if(file){
+            const form = new FormData()
+            form.append('image', file)
+            try {
+                const res = await httpRequest.patch('user/image', form)
+                if(res.status === 200){
+                    setValue('image', res.data.data.image)
+                    toast.success('Upload image successfully!')
+                } else {
+                    toast.error('Submit image faild!')
+                }
+            } catch (err){
+                toast.error(err)
+            }
+        } else {
+            toast.error('Upload faild!')
+        }
+    }
+    const onSubmit = async (data) => {
+        try {
+            const res = await httpRequest.patch('user/infor', data);
+            if(res?.status === 200) {
+                toast.success('Save success!')
+            } else {
+                toast.error('Save faild!')
+            }
+        } catch (err){
+            toast.error(err)
+        }
+    }
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await httpRequest.get('user/infor');
+                if(res?.status === 200) {
+                    for(let [key, value] of Object.entries(res.data.data)) {
+                        setValue(key, value)
+                    }
+                    setName(res.data.data.name)
+                }
+            } catch (err) {
+                toast.error(err)
+            }
+        })()
+    }, [])
+    watch()
     return (
-        <Card>
+        <Card sx={{backgroundColor: '#fef2f2', paddingX: '8px', paddingBottom: '8px'}}>
             <CardHeader
                 title="Hồ Sơ Của Tôi"
                 subheader="Quản lý thông tin hồ sơ để bảo mật tài khoản"
                 avatar={
-                    <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
-                        R
+                    <Avatar sx={{ bgcolor: '#b4282b' }} aria-label="recipe">
+                        {getValues('name')? getValues('name')[0] : 'N/A'}
                     </Avatar>
                 }
             />
             <Divider />
             <CardContent>
                 <Grid container>
-                    <Grid item xs={8}>
-                        <Grid item container>
-                            <Grid item xs={3}>
+                    <Grid item xs={9} container flexDirection={'column'} spacing={4} paddingX={4}>
+                        <Grid item container spacing={4} alignItems={'center'}>
+                            <Grid item xs={3} sx={{textAlign: 'end'}}>
                                 <Typography>Name</Typography>
                             </Grid>
                             <Grid item xs={9}>
+                            <FormControl fullWidth>
                                 <Controller
                                     control={control}
                                     name="name"
                                     render={({ field: { value, onChange } }) => (
-                                        <TextField value={value} onChange={onChange} />
+                                        <TextField value={value} onChange={(e) => {onChange(e); setName(e.target.value)}} size='small' fullWidth/>
                                     )}
                                 />
                                 {errors.name && (
                                     <FormHelperText sx={{ color: 'red' }}>{errors.name.message}</FormHelperText>
                                 )}
+                                </FormControl>
                             </Grid>
                         </Grid>
-                        <Grid item container>
-                            <Grid item xs={3}>
+                        <Grid item container spacing={4} alignItems={'center'}>
+                            <Grid item xs={3} sx={{textAlign: 'end'}}>
                                 <Typography>Email</Typography>
                             </Grid>
                             <Grid item xs={9}>
+                                <FormControl fullWidth>
                                 <Controller
                                     control={control}
                                     name="email"
                                     render={({ field: { value, onChange } }) => (
-                                        <TextField value={value} onChange={onChange} />
+                                        <TextField value={value} onChange={onChange} size='small' fullWidth/>
                                     )}
                                 />
                                 {errors.email && (
                                     <FormHelperText sx={{ color: 'red' }}>{errors.email.message}</FormHelperText>
                                 )}
+                                </FormControl>
                             </Grid>
                         </Grid>
-                        <Grid item container>
-                            <Grid item xs={3}>
+                        <Grid item container spacing={4} alignItems={'center'}>
+                            <Grid item xs={3} sx={{textAlign: 'end'}}>
                                 <Typography>Phone Number</Typography>
                             </Grid>
                             <Grid item xs={9}>
+                            <FormControl fullWidth>
                                 <Controller
                                     control={control}
                                     name="phone_number"
                                     render={({ field: { value, onChange } }) => (
-                                        <TextField value={value} onChange={onChange} />
+                                        <TextField value={value} onChange={onChange} size='small' fullWidth/>
                                     )}
                                 />
                                 {errors.phone_number && (
                                     <FormHelperText sx={{ color: 'red' }}>{errors.phone_number.message}</FormHelperText>
                                 )}
+                                </FormControl>
                             </Grid>
                         </Grid>
-                        <Grid item container>
-                            <Grid item xs={3}>
+                        <Grid item container spacing={4} alignItems={'center'}>
+                            <Grid item xs={3} sx={{textAlign: 'end'}}>
                                 <Typography>Gender</Typography>
                             </Grid>
                             <Grid item xs={9}>
+                            <FormControl fullWidth>
                                 <Controller
                                     control={control}
-                                    name="gender"
+                                    name="sex"
                                     render={({ field: { value, onChange } }) => (
                                         <RadioGroup
                                             defaultValue="female"
@@ -125,44 +190,68 @@ const ProfileComponent = () => {
                                         </RadioGroup>
                                     )}
                                 />
-                                {errors.gender && (
-                                    <FormHelperText sx={{ color: 'red' }}>{errors.gender.message}</FormHelperText>
+                                {errors.sex && (
+                                    <FormHelperText sx={{ color: 'red' }}>{errors.sex.message}</FormHelperText>
                                 )}
+                                </FormControl>
                             </Grid>
                         </Grid>
-                        <Grid item container>
-                            <Grid item xs={3}>
+                        <Grid item container spacing={4} alignItems={'center'}>
+                            <Grid item xs={3} sx={{textAlign: 'end'}}>
                                 <Typography>Birthday</Typography>
                             </Grid>
                             <Grid item xs={9}>
+                            <FormControl fullWidth>
                                 <Controller
                                     control={control}
-                                    name="birthday"
+                                    name="date_of_birth"
                                     render={({ field: { value, onChange } }) => (
                                         <DatePicker
-                                            value={value}
+                                            value={dayjs(value)}
                                             onChange={onChange}
                                             defaultValue={dayjs()}
                                             onError={(err) => {
-                                                console.log(err);
-                                                setError('birthday', { type: 'custom', message: err });
+                                                setError('date_of_birth', { type: 'custom', message: err });
                                             }}
                                             slotProps={{
                                                 textField: {
-                                                    helperText: errors?.birthday?.message,
+                                                    helperText: errors?.date_of_birth?.message,
                                                 },
                                             }}
                                         />
                                     )}
                                 />
+                                </FormControl>
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Avatar />
+                    
+                    <Grid item xs={3} sx={{
+                        display: 'flex', 
+                        flexDirection: 'column',
+                        gap: 2,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderLeft: 'solid 1px #0000001f'
+                        }}>
+                        <Avatar src={getValues('image')} sx={{height: 100, width: 100}}/>
+                        <input type='file' ref = {inputRef} onChange={uploadImage} style={{display: 'none'}} accept='image/*'/>
+                        <Button 
+                        variant='outlined'
+                        onClick={() => {
+                            inputRef.current.click()
+                        }}
+                        sx={{textTransform: 'none'}}
+                        >Upload Photo</Button>
+                        {errors.image && (
+                            <FormHelperText sx={{ color: 'red' }}>{errors.image.message}</FormHelperText>
+                        )}
                     </Grid>
                 </Grid>
             </CardContent>
+            <CardActions sx={{justifyContent: 'center'}}>
+                <Button sx={{textTransform: 'none'}} variant='contained' onClick={handleSubmit(onSubmit)}>Save</Button>
+            </CardActions>
         </Card>
     );
 };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,38 +1,52 @@
 import { Grid, Typography, Divider, MenuList, MenuItem } from '@mui/material';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import PersonIcon from '@mui/icons-material/Person';
 import ProfileComponent from '~/components/profile/ProfileComponent';
-import { useEffect } from 'react';
-import httpRequest from 'src/utils/httpRequest';
+import {useState} from 'react'
+import EditIcon from '@mui/icons-material/Edit';
 
 function Profile() {
-    useEffect(() => {
-        (async () => {
-            const res = await httpRequest.get('user/infor');
-            console.log(res);
-        })();
-    }, []);
+    const [name, setName] = useState()
+    const [tab, setTab] = useState(0)
+    
     return (
-        <Grid container sx={{ height: '100vh', justifyContent: 'center', marginY: '30px' }}>
-            <Grid item xs={2}></Grid>
+    <Grid container sx={{ height: '100vh', justifyContent: 'center', marginY: '30px' }} spacing={1}>
+            <Grid item xs={1}></Grid>
             <Grid item xs={2} container flexDirection={'column'}>
-                <Grid item container gap="12px">
-                    <AccountCircleOutlinedIcon fontSize="large" />
-                    <Typography>Thuy Trang</Typography>
+                <Grid item container gap="12px" padding={'11px'} alignItems={'center'}>
+                    <EditIcon sx={{height: 50, width: 'auto'}}/>
+                    <div>
+                    <Typography color='#0009'> Edit Profile</Typography>
+                    <Typography fontWeight={600} fontSize={14}>{name}</Typography>
+                    </div>
                 </Grid>
                 <Divider />
                 <MenuList>
-                    <MenuItem sx={{ display: 'flex', gap: '8px' }}>
+                    <MenuItem sx={{ display: 'flex', gap: '8px' }} onClick={() => setTab(0)}>
                         <PersonIcon /> My Account
                     </MenuItem>
-                    <MenuItem sx={{ paddingLeft: '46px' }}>Profile</MenuItem>
-                    <MenuItem sx={{ paddingLeft: '46px' }}>Address</MenuItem>
+                    <MenuItem sx={{ paddingLeft: '46px', color: tab === 0? '#b4282b' : '#161823'}}
+                    onClick={() => setTab(0)}
+                    >Profile</MenuItem>
+                    <MenuItem sx={{ paddingLeft: '46px', color: tab === 1? '#b4282b' : '#161823' }}
+                    onClick={() => setTab(1)}
+                    >Address</MenuItem>
                 </MenuList>
             </Grid>
-            <Grid item xs={6}>
-                <ProfileComponent />
+            <Grid item xs={8}>
+                {(() => {
+                    switch (tab){
+                        case 0: 
+                            return (
+                                <ProfileComponent setName = {setName}/>
+                            );
+                        default: 
+                            return (
+                                <ProfileComponent />
+                            );
+                    }
+                })()}
             </Grid>
-            <Grid item xs={2}></Grid>
+            <Grid item xs={1}></Grid>
         </Grid>
     );
 }
